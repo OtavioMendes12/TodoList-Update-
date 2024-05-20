8d71b9efbce74f39333beddf76a0a60d63e739aa
Otávio Mendes <102169915+OtavioMendes12@users.noreply.github.com>
2024-05-20 20:57:03 -0300

    testes unitarios

---
+++ src/test/java/com/otaviomendes/TodoList/integration/TodoServiceIntegrationTest.java
@@ -0,0 +1,106 @@
+package com.otaviomendes.TodoList.integration;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.otaviomendes.TodoList.entity.Prioridade;
+import com.otaviomendes.TodoList.service.TodoService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.otaviomendes.TodoList.entity.Todo;
+import com.otaviomendes.TodoList.repository.TodoRepository;
+
+public class TodoServiceIntegrationTest {
+
+	private TodoService todoService;
+
+	@Mock
+	private TodoRepository todoRepository;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.initMocks(this);
+		todoService = new TodoService(todoRepository);
+	}
+
+	@Test
+	void testCreate() {
+		Todo todo = new Todo();
+		todo.setPrioridade(Prioridade.Alta);
+		when(todoRepository.save(any(Todo.class))).thenReturn(todo);
+		when(todoRepository.findAll()).thenReturn(Arrays.asList(todo));
+
+		List<Todo> result = todoService.create(todo);
+		assertNotNull(result);
+		assertEquals(1, result.size());
+		assertEquals(Prioridade.Media, result.get(0).getPrioridade());
+
+		verify(todoRepository).save(todo);
+		verify(todoRepository).findAll();
+	}
+
+	@Test
+	void testList() {
+		Todo todo1 = new Todo();
+		todo1.setPrioridade(Prioridade.Alta);
+		Todo todo2 = new Todo();
+		todo2.setPrioridade(Prioridade.Alta);
+		when(todoRepository.findAll()).thenReturn(Arrays.asList(todo2, todo1));
+
+		List<Todo> result = todoService.list();
+		assertNotNull(result);
+		assertEquals(2, result.size());
+		assertEquals(Prioridade.Alta, result.get(0).getPrioridade());  // Verifica se a ordenação está correta
+
+		verify(todoRepository).findAll();
+	}
+
+	@Test
+	void testUpdate() {
+		Todo existingTodo = new Todo();
+		existingTodo.setId(1L);
+		existingTodo.setPrioridade(Prioridade.Media);
+		Todo updatedTodo = new Todo();
+		updatedTodo.setId(1L);
+		updatedTodo.setPrioridade(Prioridade.Alta);
+
+		when(todoRepository.save(any(Todo.class))).thenReturn(updatedTodo);
+		when(todoRepository.findAll()).thenReturn(Arrays.asList(updatedTodo));
+
+		List<Todo> result = todoService.update(1L, updatedTodo);
+		assertNotNull(result);
+		assertEquals(1, result.size());
+		assertEquals(Prioridade.Alta, result.get(0).getPrioridade());
+
+		verify(todoRepository).save(updatedTodo);
+		verify(todoRepository).findAll();
+	}
+
+	@Test
+	void testDelete() {
+		Todo todo1 = new Todo();
+		todo1.setId(1L);
+		todo1.setPrioridade(Prioridade.Alta);
+		Todo todo2 = new Todo();
+		todo2.setId(2L);
+		todo2.setPrioridade(Prioridade.Alta);
+
+		when(todoRepository.findAll()).thenReturn(Arrays.asList(todo1), Arrays.asList());
+
+		List<Todo> initialResult = todoService.list();
+		assertEquals(1, initialResult.size());
+
+		List<Todo> resultAfterDeletion = todoService.delete(1L);
+		assertTrue(resultAfterDeletion.isEmpty());
+
+		verify(todoRepository).deleteById(1L);
+		verify(todoRepository, times(2)).findAll();
+	}
+
+}
---
+++ src/test/java/com/otaviomendes/TodoList/unit/TodoServiceUnitTest.java
@@ -0,0 +1,76 @@
+package com.otaviomendes.TodoList.unit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.otaviomendes.TodoList.entity.Prioridade;
+import com.otaviomendes.TodoList.entity.TipoTodo;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import com.otaviomendes.TodoList.entity.Todo;
+
+public class TodoServiceUnitTest {
+
+    @Test
+    void testPriorityOrdering() {
+
+        Todo highPriorityTodo = new Todo();
+        highPriorityTodo.setPrioridade(Prioridade.Alta);
+
+        Todo mediumPriorityTodo = new Todo();
+        mediumPriorityTodo.setPrioridade(Prioridade.Media);
+
+        Todo lowPriorityTodo = new Todo();
+        lowPriorityTodo.setPrioridade(Prioridade.Baixa);
+
+
+        List<Todo> todos = Arrays.asList(mediumPriorityTodo, lowPriorityTodo, highPriorityTodo);
+
+        todos.sort(Comparator.comparing(todo -> {
+            switch (todo.getPrioridade()) {
+                case Alta:
+                    return 1;
+                case Media:
+                    return 2;
+                case Baixa:
+                    return 3;
+                default:
+                    return 4;
+            }
+        }));
+
+        assertEquals(Prioridade.Alta, todos.get(0).getPrioridade());
+        assertEquals(Prioridade.Media, todos.get(1).getPrioridade());
+        assertEquals(Prioridade.Baixa, todos.get(2).getPrioridade());
+    }
+
+    @Test
+    void testTipoTodoOrdering(){
+        Todo dataTipoTodo = new Todo();
+        dataTipoTodo.setTipoTodo(TipoTodo.Data);
+        Todo prazoTipoTodo = new Todo();
+        prazoTipoTodo.setTipoTodo(TipoTodo.Data);
+        Todo livreTipoTodo = new Todo();
+        livreTipoTodo.setTipoTodo(TipoTodo.Data);
+        List<Todo> todos = Arrays.asList(livreTipoTodo, prazoTipoTodo, dataTipoTodo);
+        todos.sort(Comparator.comparing(todo -> {
+            switch (todo.getTipoTodo()) {
+                case Data:
+                    return 1;
+                case Prazo:
+                    return 2;
+                case Livre:
+                    return 3;
+                default:
+                    return 4;
+            }
+
+        }));
+        assertEquals(TipoTodo.Data, todos.get(0).getTipoTodo());
+        assertEquals(TipoTodo.Prazo, todos.get(0).getTipoTodo());
+        assertEquals(TipoTodo.Livre, todos.get(0).getTipoTodo());
+
+    }
+}
