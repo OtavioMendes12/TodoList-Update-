a2025e56a663536d1e7f7ee98312bd92ab628a9c
Otávio Mendes <102169915+OtavioMendes12@users.noreply.github.com>
2024-06-23 16:15:24 -0300

    updates

---
+++ front-end/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"W9N9oLOghtHEQOzbl8wh8gkUz7LTPcf/3sUNDZPMhRE=\"}"
--- front-end/src/app/todo/page.js
+++ front-end/src/app/todo/page.js
@@ -4,14 +4,37 @@ import ListaTarefas from "../../components/ListaTarefas/listaTarefas";
 import { Typography } from "@mui/material";
 import '@fontsource/roboto/700.css';
 import ButtonCreate from "../../components/ButtonCreate/ButtonCreate";
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(timer);
+  }, []);
+
+  const formatDate = (date) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString(undefined, options);
+  };
+
+  const formatTime = (date) => {
+    return date.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+  };
+
   return (
-<>
-    <div style={{height: '20vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}} >
-      <Typography variant="h4">Todo List</Typography>
-    </div>
-    <ListaTarefas />    
-</>
+    <>
+      <div style={{height: '50vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', fontFamily: 'Calibri'}} >
+        <Typography variant="h4">TODO LIST</Typography>
+        <Typography variant="h6">{formatDate(dateTime)} {formatTime(dateTime)}</Typography>
+        <Typography variant="h4">OTÁVIO MENDES</Typography>
+      </div>
+      <ListaTarefas />
+    </>
   );
 }
